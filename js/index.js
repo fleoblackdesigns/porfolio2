1d4331e94f9f01a14cffe89a716068df28fcaa97
L BLACKWOOD <102272673+bbdmaster@users.noreply.github.com>
2022-11-12 06:59:37 -0500

    accodian

--- js/index.js
+++ js/index.js
@@ -7,3 +7,27 @@ var displayText = document.getElementById("displayText");
 console.log(displayText);
 displayText.textContent = HelloWorld; 
 
+//Accordion Section
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+//Animate Element
+$(document).ready(function(){
+    $("button").click(function(){
+      var p = $("p");  
+      p.animate({left: '100px'}, "slow");
+      p.animate({fontSize: '3em'}, "slow");
+    });
+  });
